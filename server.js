cc247830551af5dca8fb1eabbbf5af5e6ae77eb2
Dishant1-git <Dishantkaushal456@gmail.com>
2026-05-08 14:53:33 +0530

    up

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-const nodemailer=require('nodemailer')
+const { MailerSend, EmailParams, Sender, Recipient } = require("mailersend");
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const bcrypt = require("bcrypt")
@@ -123,39 +123,39 @@ mongoose.connect(process.env.Mongoose_url)
 
 
 
-    //nodemailer transporter setup
-    const transporter=nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:process.env.AdminMail,
-            pass:process.env.AdminPass
-        }
-    })
+    //mailersend setup
+    const mailerSend = new MailerSend({
+        apiKey: process.env.mailtoken ,
+    });
 
 
-//nodemailer api
+//mailersend api
 
 app.post('/api/nodemail',async(req,res)=>{
 
-    console.log("nodemailer api hit")
+    console.log("mailersend api hit")
 const email=req.body.email
 const name=req.body.name
 console.log(email,"email from api")
 
-const mailOptions={
-    from:"<No reply>"+process.env.Email,
-    to:email,
-    subject:"Regarding your complaint",
-    text:"Thanks for contacting us, we have received your complaint and will get back to you soon",
-    html:`<h1>Hi ${name} </h1>   <h3> Sorry for the problem You are facing.</h3><p>We have received your complaint and will Resolve your problem as soon as possible. </p>`
-}
-const result= await transporter.sendMail(mailOptions)
-console.log(result,"result from nodemailer")
+const sentFrom = new Sender(process.env.maileremail || "no-reply@yourdomain.com", "Municipal Services");
+const recipients = [
+  new Recipient(email, name)
+];
 
-if(result){
+const emailParams = new EmailParams()
+  .setFrom(sentFrom)
+  .setTo(recipients)
+  .setSubject("Regarding your complaint")
+  .setText("Thanks for contacting us, we have received your complaint and will get back to you soon")
+  .setHtml(`<h1>Hi ${name} </h1>   <h3> Sorry for the problem You are facing.</h3><p>We have received your complaint and will Resolve your problem as soon as possible. </p>`);
+
+try {
+    const result = await mailerSend.email.send(emailParams);
+    console.log(result,"result from mailersend")
     res.send({statuscode:1,message:"Email sent Succesfully"})
-}
-else{
+} catch (error) {
+    console.log(error,"error from mailersend")
     res.send({statuscode:0,message:"Error ocuured while Sending mail"})
 }
 
